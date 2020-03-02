fb98749a2337b8d5e7913154b079c1d769ca6dde
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-03-02 21:23:36 +0200

    Add files via upload

---
+++ JavaCore 1208 Freedom of the press/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+/*
+
+1208 Freedom of the press
+Write five print methods with different parameters.
+Requirements:
+1. The program should not display text on the screen.
+2. The Solution class must contain six methods.
+3. The Solution class must contain the static main () method.
+4. The Solution class should contain five static print () methods with different parameters.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String s) {
+
+    }
+
+    public static void print(int s) {
+    }
+
+    public static void print(boolean s) {
+    }
+    public static void print(float s) {
+
+    }
+
+    public static void print(double s) {
+
+    }
+}
